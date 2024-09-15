02ffb6a269842336f7fbe6619c3d02f3c53e06ad
EugineI <eugineindimuli@gmail.com>
2024-09-15 20:24:35 +0300

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for letter in my_string:
-        if letter == 'c' or letter == 'C':
-            continue
-        else:
+        if letter != 'c' and letter != 'C':
             new_string += letter
     return(new_string)
