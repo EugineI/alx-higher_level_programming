51bed0955a83d88a426d51ee7c51738352900a08
EugineI <eugineindimuli@gmail.com>
2024-10-02 14:23:03 +0300

    docstring2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+"""
+This module handles basic square operations
+
+It will contain functions and classes to handle calculation of the square area.
+"""
+
+
 class Square:
+    """
+    this class defines a square
+    """
     def __init__(self, size=0):
+        """
+        This function initialises the attributes
+        size: int
+        length of the squares side
+        raises:
+        TypeError and ValueError
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
