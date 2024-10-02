287b2fdcc7f2e76bbb39370e593e28a4392aaa7c
EugineI <eugineindimuli@gmail.com>
2024-10-02 14:28:03 +0300

    docstring3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+This module handles basic square operations
+
+It will contain functions and classes to handle calculation of the square area.
+"""
+
+
 class Square:
+    """
+    This class defines a square and finds its area
+    """
     def __init__(self, size=0):
+        """
+        initialises the attributes
+        size: int
+        length of the squares side
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -8,6 +23,11 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        finds area of the square
+        Return:
+        area of the square
+        """
         length = self.__size
         area = length ** 2
         return area
