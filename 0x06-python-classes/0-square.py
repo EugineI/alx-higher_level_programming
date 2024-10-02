a183e52692b023747a2fd6079d701ebe6900ce9b
EugineI <eugineindimuli@gmail.com>
2024-10-02 10:35:58 +0300

    docstring0.1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+This module handles basic square operations
+
+It will contain functions and classes to handle calculation of the square area.
+"""
 class Square:
     """
-    An empty class to define a squere.
+    An empty class to define a square.
 
     This class has no attributes yet.
     """
