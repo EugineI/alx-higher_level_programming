f7e6de986c418f6b070d4b5285dd807abf2dbd3c
EugineI <eugineindimuli@gmail.com>
2024-10-02 14:41:18 +0300

    docstring5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
+"""                                                                     This module handles basic square operations                                                                                                     It will contain functions and classes to handle calculation of the square area.
+"""
+
+
 class Square:
+    """
+    defines a square
+    """
     def __init__(self, size=0):
+        """
+        initialises the attribute size
+        """
         self.__size = size
 
     @property
     def size(self):
+        """
+        retrieves the attribute size
+        """
         length = self.__size
         return length
 
     @size.setter
     def size(self, value):
+        """
+        sets the attribute size to value
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -17,10 +33,16 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Return: area of the square
+        """
         area = self.__size ** 2
         return area
 
     def my_print(self):
+        """
+        prints the square using '#'
+        """
         if self.__size == 0:
             print()
         for i in range(self.__size):
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
+"""                                                                     This module handles basic square operations                                                                                                     It will contain functions and classes to handle calculation of the square area.
+"""
+
+
 class Square:
+    """
+    Defines a square
+    """
     def __init__(self, size=0, position=(0, 0)):
+        """
+        initialises attributes size and position
+        position: turple
+        size: int
+        """
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
+        """
+        retrieves size
+        """
         length = self.__size
         return length
 
     @size.setter
     def size(self, value):
+        """
+        sets size to value
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -19,20 +37,32 @@ class Square:
 
     @property
     def position(self):
+        """
+        retrieves position
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
+        """
+        sets position to value
+        """
         if not isinstance(value, tuple) or len(value) != 2 or \
            not all(isinstance(num, int) and num >= 0 for num in value):
                raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
+        """
+        Return: area of the square
+        """
         area = self.__size ** 2
         return area
 
     def my_print(self):
+        """
+        prints square
+        """
         if self.__size == 0:
             print("")
         for _ in range(self.__position[1]):
