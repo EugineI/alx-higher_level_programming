f7ef5e81f3f475d7348c761f1d2fc9311ce6e815
EugineI <eugineindimuli@gmail.com>
2024-10-02 14:19:09 +0300

    docstring1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,6 +4,8 @@ This module handles basic square operations
 
 It will contain functions and classes to handle calculation of the square area.
 """
+
+
 class Square:
     """
     An empty class to define a square.
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""
+This module handles basic square operations
+
+It will contain functions and classes to handle calculation of the square area.
+"""
+
+
 class Square:
+    """
+    This class defines a square
+    """
     def __init__(self, size):
+        """
+        Initializes all the attributes needed
+
+        size : int
+            the length of the square
+        """
         self.__size = size
