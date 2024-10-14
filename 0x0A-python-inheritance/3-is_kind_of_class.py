79176da57b988f12fe241993c48e31488b353b04
EugineI <eugineindimuli@gmail.com>
2024-10-14 21:00:03 +0300

    inheritance1

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,7 @@ check is instance or object is an instance of the class
 or of its child class
 """
 def is_kind_of_class(obj, a_class):
+    """
+    checks if is instance to child or parent
+    """
     return isinstance(obj, a_class)
