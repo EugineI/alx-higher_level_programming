980004e60bd15ae6bc32c437167c7555ec71b1e1
EugineI <eugineindimuli@gmail.com>
2024-11-22 00:23:34 +0300

    dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,6 @@
 SELECT tv_genres.name AS name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE show_id = 8
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
 ORDER BY name ASC;
