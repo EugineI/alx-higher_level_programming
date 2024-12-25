2c8cc38990dfb43aa1c9655ca42f29950f80d9de
EugineI <eugineindimuli@gmail.com>
2024-12-25 21:08:40 +0300

    list of states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+this script lists all the states from the database hbtn_0c_0_usa
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database
+            )
+    cursor = db.cursor()
+    state_list = """
+    SELECT * FROM states ORDER BY id ASC;
+    """
+    cursor.execute(state_list)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
