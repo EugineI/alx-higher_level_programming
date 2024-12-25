c38002a0168a892ea0d91ed415e4a116b3f6512a
EugineI <eugineindimuli@gmail.com>
2024-12-25 23:31:12 +0300

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,6 @@ lists states starting with N from hbtn_0c_0_usa
 import MySQLdb
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -18,7 +16,7 @@ if __name__ == "__main__":
             )
     cursor = db.cursor()
     state_list = """
-    SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC
+    SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC;
     """
     cursor.execute(state_list)
     states = cursor.fetchall()
