87f506c04475001b5a5739104e298207327165df
EugineI <eugineindimuli@gmail.com>
2024-12-26 23:53:35 +0300

    formatting

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     username = sys.argv[1]
-    password = sys.argv[2]
+    password = sys.argv[2].replace('@', '%40')
     database = sys.argv[3]
 
     engine = create_engine(
@@ -27,5 +27,5 @@ if __name__ == '__main__':
     states = session.query(State).order_by(State.id.asc()).all()
 
     for state in states:
-        print("{}:{}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
