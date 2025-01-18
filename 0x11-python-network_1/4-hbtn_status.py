62d81d9c412c6bde397e267bab8eb592146c0947
EugineI <eugineindimuli@gmail.com>
2025-01-18 13:28:23 +0300

    requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,12 +2,13 @@
 """
 fetch a given url
 """
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as status:
-        content = status.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(content.decode('utf-8'))))
-        print('\t- content: {}'.format(content.decode('utf-8')))
+    
+    response = requests.get(url)
+    content = response.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
